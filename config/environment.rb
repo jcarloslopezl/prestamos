a2a0d33526c9769743a4c05c5ed90eba360463c3
Juan Carlos López <jcarloslopezl@hotmail.com>
2016-11-01 01:47:07 -0600

    Configura omniauth initializer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Facebook APP
+ENV['FACEBOOK_KEY'] = "158930057905379"
+ENV['FACEBOOK_SECRET'] = "f0ed04e953396dc9227dbdf2de78bb09"
