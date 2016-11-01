0e8958519bc996e3e682baa79ce77def8554c15a
Juan Carlos López <jcarloslopezl@hotmail.com>
2016-11-01 01:59:19 -0600

    Agrega vistas home

---
+++ db/migrate/20161101051725_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :second_name
+      t.string :second_last_name
+      t.string :first_last_name
+      t.datetime :birth_date
+      t.string :curp
+      t.string :rfc
+      t.string :gender
+      t.string :birth_state
+      t.integer :phone_number
+      t.string :uid
+
+      t.timestamps
+    end
+  end
+end
