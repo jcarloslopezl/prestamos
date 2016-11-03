fd64d8fec13c4d245edd6a90e146bd99e59f08c8
Juan Carlos López <jcarloslopezl@hotmail.com>
2016-11-03 05:03:29 -0600

    Agrega validaciones para actualización de perfil

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,18 @@ class SessionsController < ApplicationController
     session[:omniauth] = auth
     user = User.sign_in_from_omniauth auth
     session[:user_id] = user.id
-    redirect_to root_url, notice: "SIGNED IN"
+
+    if user
+      redirect_to root_url, notice: "Ha iniciado sesión correctamente!"
+    else
+      redirect_to root_url, notice: "No se pudo iniciar la sesión!"
+    end
+    
   end
 
   def destroy
    session[:user_id] = nil
    session[:omniauth] = nil
-   redirect_to root_url, notice: "SIGNED OUT"
+   redirect_to root_url, notice: "Ha cerrado su sesión correctamente!"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+	before_action :set_user, except: [:destroy]
 	
   def index
+  end
+
+  def show
   end
 
   def profile
@@ -8,6 +12,30 @@ class UsersController < ApplicationController
   end
 
   def update
-  	
+  	if @user.update(user_params)
+  		redirect_to @user, notice: 'Se han actualizado sus datos correctamente.'
+  	else
+  		render :edit
+  	end
   end
+
+  private
+
+  def set_user
+  	@user = User.find(session[:user_id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, 
+    														 :second_name, 
+    														 :second_last_name, 
+    														 :first_last_name, 
+    														 :birth_date, 
+    														 :curp, 
+    														 :rfc, 
+    														 :gender, 
+    														 :birth_state, 
+    														 :phone_number)
+  end
+
 end
