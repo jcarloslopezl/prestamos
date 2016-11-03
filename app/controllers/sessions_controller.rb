c112d2286b46e10078e915a09c6bd94f840726bc
Juan Carlos López <jcarloslopezl@hotmail.com>
2016-11-03 00:27:30 -0600

    Agrega controlador para manejo de sesión con facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    session[:omniauth] = auth.except 'extra'
+    session[:omniauth] = auth
     user = User.sign_in_from_omniauth auth
     session[:user_id] = user.id
     redirect_to root_url, notice: "SIGNED IN"
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  # provider :developer unless Rails.env.production?
+  # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, "158930057905379", "f0ed04e953396dc9227dbdf2de78bb09"
 end
